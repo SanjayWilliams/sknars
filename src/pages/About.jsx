129d43df53763e55baacf50e1e9c0315b4096d9d
Sanjay Williams <sanjmdw@gmail.com>
2022-09-11 20:00:25 -0400

    Fixed up About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,68 @@ function About() {
             <h1 className="about-title">About</h1>
             <div className="about-backdrop"></div>
             <img className="about-img" src="radiomic.jpg"/>
-            <p className="about-body">The society was founded in 1978 by Erickson France and other members. It was booming, many persons were interested in joining the society </p>
+            <p className="about-body">The St. Kitts-Nevis-Anguilla Amateur Radio Society was launched on 7th November 1973 at &:00p.m. local time. The Chairman at this meeting was Lloyd Mac Pennyfeather, VP2KP, and venue was the "old" Red Cross building situated on Horsford Road, Basseterre.
+
+            The formation, which took place at the Birdrock QTH (residence) of Sid May, VP2KH, was attended by the following Radio Amateurs:<br/><br/>
+
+            <strong>
+            Lloyd Mac Pennyfeather VP2KP<br/>
+
+            Val Manchester VP2KV<br/>
+
+            Terrence Nisbett VP2KQ<br/>
+
+            Randolph "Lou" Edmeade VP2KX<br/>
+
+            Glen Dear VP2SK<br/>
+
+            Sid May  VP2KH also G4CTQ<br/><br/>
+
+            </strong>
+
+            Following this formation, a meeting was convened at the Red Cross Headquarters on Horsford Road, Basseterre.
+
+            The St. Kitts-Nevis-Anguilla Amateur Radio Society was formally founded at 00:45 GMT on the 12th April 1973
+
+            The group then formed a Committee as follows:<br/><br/>
+
+            <strong>
+            Lloyd Mac Pennyfeather appointed Secretary <br/>
+
+            "Lou" Edmeade appointed Treasurer <br/>
+
+            Val Manchester appointed Editor and Publicity Manager <br/>
+
+            Sid May appointed Trustee <br/>
+
+            Terry Nisbett appointed Committee Member <br/>
+
+            Glen Dear appointed Committee Member <br/><br/></strong>
+
+            Application for a Radio Amateur Licence was made by Secretary R. "Lou" Edmeade by letter dated October 26th 1973 to Permanent Secretary, Ministry of Communications and Works, Government Headquarters; a callsign was suggested.<br/>
+
+            The Secretary again made application dated: 28th August 1975; once again on 23 September 1975; and yet again 5th February 1976.
+
+            On 11th May 1976 licence was granted in response to the letter of 28 August 1975 by Telecommunications Officer (Ag.) Zephaniah "Syl" A. Joseph. The Society's Callsign: VP2KR<br/><br/>
+
+            
+
+            In the years that followed, the Society assisted in times of natural and "man-made" emergencies in its role of providing the community with communications in the events be it road races to hurricane. The governmental department which dealt with natural emergencies was the Disaster Preparedness Unit. This was continued when the National Emergency
+             Management Agency N.E.M.A. was introduced. The Society resolved to assist N.E.M.A. with personnel and equipment when required.<br/><br/>
+
+            
+
+            It soon became necessary to record the Society Procedures in the event of National Emergencies. This task was undertaken by Asquith "Al" Elmes V44KAE and he produced a detailed document mapping out the various steps in the Society's emergency communications response.
+
+            It was also necessary to train a cadre of "radio operators" who N.E.M.A. could call upon to operate its radio communications facility.
+
+            In addition, licensed radio amateurs would be deployed at N.E.M.A's Headquarters from the S.K.N.A.A.R.S.
+
+            S.K.N.A.A.R.S. volunteers assist N.E.M.A. in maintenance of its radio facility as an ongoing contribution to community service.<br/><br/>
+
+
+            <strong>The name St. Kitts-Nevis-Anguilla Amateur Radio Society was officially changed to St. Kitts-Nevis Amateur Radio Amateur Society in February 2022 by motion by the Membership at the Annual General Meeting</strong>
+            </p>
 
         </div>
     )
