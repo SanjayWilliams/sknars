bcb1c6f2b474296a750fa249a3dfc25f3c3042e4
Sanjay Williams <sanjmdw@gmail.com>
2022-09-12 12:45:04 -0400

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,10 +7,10 @@ function Nav() {
     <nav className="nav">
         <img src="../logo.png" className="logo" />
         <ul className="nav-menu">
-            <li className="nav-menu-item"><Link to="/">Home</Link></li>
-            <li className="nav-menu-item"><Link to="/members">Members</Link></li>
-            <li className="nav-menu-item"><Link to="/about">About</Link></li>
-            <li className="nav-menu-item"><Link to="/contact">Contact</Link></li>
+            <li className="nav-menu-item"><Link style={{ textDecoration: 'none', color: 'black' }} to="/">Home</Link></li>
+            <li className="nav-menu-item"><Link style={{ textDecoration: 'none', color: 'black' }} to="/members">Members</Link></li>
+            <li className="nav-menu-item"><Link style={{ textDecoration: 'none', color: 'black' }} to="/about">About</Link></li>
+            <li className="nav-menu-item"><Link style={{ textDecoration: 'none', color: 'black' }} to="/contact">Contact</Link></li>
         </ul>
     </nav>
     )
