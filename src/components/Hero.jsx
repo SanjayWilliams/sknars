e79d979b6dedce56d768bad292ea67e45151c349
Sanjay Williams <sanjmdw@gmail.com>
2022-09-11 20:30:28 -0400

    Added some CSS changes to Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     return (
         <div className="hero-container">
             <div className="hero-text-container">
-                <h1 className="hero-title">Welcome to SKNARS, Amateur Radio Station V44KR</h1>
+                <h1 className="hero-title">Welcome to SKNARS, Amateur Radio Station <span className="call-sign">V44KR</span></h1>
                 <div>
                     <Link style={{ textDecoration: 'none' }} className="btn btn-hero" to="/about">About</Link>
                     <Link style={{ textDecoration: 'none' }} className="btn btn-hero" to="/contact">Contact</Link>
