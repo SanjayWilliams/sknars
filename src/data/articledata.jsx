50624fa42dd5037829c76040e09871c206cecfc7
Sanjay Williams <sanjmdw@gmail.com>
2022-12-05 21:27:22 -0400

    Update articledata.jsx

--- src/data/articledata.jsx
+++ src/data/articledata.jsx
@@ -1,7 +1,7 @@
 const articledata = [
     {
         id: 1,
-        title:"Message from President V44PATPatricia",
+        title:"Message from President V44PAT Patricia",
         text: <p>As we stand on the threshold of the 50th anniversary of St. Kitts-Nevis Amateur Radio Society S.K.N.A.R.S., it is only fitting to look back at the past while considering the path ahead.<br></br> <br></br>Firstly, I take this opportunity to thank the founding members for their foresight in creating this Society which, even with its limited resources, still continue to exist.<br></br><br></br>Our outlook for the future of the Society is to establish our main headquarters which is pivotal in achieving many if not most of our dreams which include but not limited to:<br></br><br></br>Training Facility <br></br>functional unit that better enable the Society to respond during emergencies. <br></br><br></br>Therefore, it is easy to deduce that our work is cut out for us.</p>,
         image:"./president1.jpeg",
         author:"President",
